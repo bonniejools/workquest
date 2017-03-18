551d235cbce98cd7b49d8195054e8250e9b2a0b2
Jonathan Balls <j.balls@btopenworld.com>
2017-03-18 21:52:15 +0000

    Task move hooks on frontend

--- static/js/workquest.js
+++ static/js/workquest.js
@@ -1,6 +1,10 @@
 $( function() {
-        $( ".tasks" ).sortable({
-                  connectWith: ".tasks",
-                  receive: (event, ui) => console.log(ui.item)
-                          }).disableSelection();
-          } );
+    $( ".tasks" ).sortable({
+              connectWith: ".tasks",
+              receive: (event, ui) => {
+                  var task_id = ui.item.attr("task_id");
+                  var moved_list = ui.item.parent().attr("list_name");
+                  console.log("Task number " + task_id + " moved to " + moved_list);
+              }
+      }).disableSelection();
+} );
