5f259733ee8d5e39edf67bd6827caec619b09bb5
Jonathan Balls <j.balls@btopenworld.com>
2017-03-18 20:02:39 +0000

    Damage and gold

--- templates/js/workquest.js
+++
@@ -1,6 +0,0 @@
-$( function() {
-        $( ".tasks" ).sortable({
-                  connectWith: ".tasks",
-                  receive: (event, ui) => console.log(ui.item)
-                          }).disableSelection();
-          } );
