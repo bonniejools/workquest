85dfd69eed681106a414bf0621ad7d5bb23c3b5b
Siokhan <siokhank@gmail.com>
2017-03-19 00:26:52 +0000

    adding upgrade and level up logic, also changed the final tier items to have to upgrade cost

---
+++ mekonix.js
@@ -0,0 +1,47 @@
+
+var items = require('./items.json')
+
+function levelUp (exp){
+    var level = Math.floor(exp/100);
+    return level;
+}
+
+function upgrade (item) {
+    var tier = getItem(item).tier + 1;
+    var type = getItem(item).type;
+    var tmp = null;
+    items.forEach((item)=> {
+        if (item.type == type && item.tier == tier){
+            tmp = item
+        }
+    })
+    if(tmp == null)
+        return item
+    else
+        return tmp
+}
+
+function canUpgrade (gold, item){
+    var needToPay = getItem(item).cost;
+    if (gold >= needToPay) {
+        var newItem = upgrade(item);
+        gold -= needToPay;
+        console.log("Item has been upgraded\n");
+    }else{
+        console.log("Not enough gold to upgrade desired item!\n");
+    }
+    return{gold:gold, item:newItem};
+}
+
+function getItem(name)
+{
+    var tmp = null
+    items.forEach((item)=>
+        {
+            if(item.name == name)
+                tmp = item
+        })
+    return tmp
+}
+
+module.exports = {levelUp, upgrade, canUpgrade, getItem}
